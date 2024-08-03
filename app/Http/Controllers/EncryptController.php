12ae6af9cff2a083c52888102e1cd2f0a9399f38
sirwez <weslley.aquinog12@gmail.com>
2024-08-03 16:49:55 -0300

    substitui bcrypt por encrypt e decrypt

--- app/Http/Controllers/EncryptController.php
+++ app/Http/Controllers/EncryptController.php
@@ -8,16 +8,29 @@ use Illuminate\Http\Request;
 
 class EncryptController extends Controller
 {
-    public function postCard(EncryptRequest $request){
-
-        $document = $request->userDocument;
-        $card = $request->creditCardToken;
+    public function postCard(EncryptRequest $request)
+    {
+        // Criptografar o documento do usuário e o token do cartão de crédito
+        $document = \encrypt($request->userDocument);
+        $card = \encrypt($request->creditCardToken);
         $value = $request->value;
-        $Encrypt = new Encrypt();
-        $Encrypt->userDocument = $document;
-        $Encrypt->creditCardToken = $card;
-        $Encrypt->value = $value;
-        $Encrypt->save();
 
+        // Criar uma nova instância do modelo Encrypt e salvar os dados
+        $encrypt = new Encrypt();
+        $encrypt->userDocument = $document;
+        $encrypt->creditCardToken = $card;
+        $encrypt->value = $value;
+        $encrypt->save();
+
+        // Retornar uma resposta JSON
+        return response()->json([
+            'success' => true,
+            'message' => 'Dados criptografados e salvos com sucesso.',
+            'data' => [
+                'userDocument' => $encrypt->userDocument,
+                'userDocumentt' => decrypt($document),
+            ]
+        ], 201);
     }
+
 }
